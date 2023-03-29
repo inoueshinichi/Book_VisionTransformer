4bf26f6aa95a28adee1881f9ba5c5a99c97836cc
Inoue Shinichi <inoue.shinichi.1800@gmail.com>
2023-03-29 12:40:23 +0900

    [Update] edit comment

--- multi_head_attention.py
+++ multi_head_attention.py
@@ -2,10 +2,10 @@
 
 # MultiHead-Attention
 --------------------------------------------------------------------------
-Input: (B, P+1, D)
-Output: (B, P+1, H)
-Head数(k) x Self-Attention [Query_W(D,H), Key_W(D,H), Value_(D,H), Attention_Weight(B,P+1,P+1)]
-OneHead_W(kH, H): 各Self-Attentionの結合(Concatenate)出力(B, P+1, kH)を(B, P+1, H)に変換する線形演算子.
+Input: (N, T, D)
+Output: (N, T, H)
+Head数(k) x Self-Attention [Query_W(D,H), Key_W(D,H), Value_(D,H), Attention_Weight(B,T,T)]
+OneHead_W(kH, H): 各Self-Attentionの結合(Concatenate)出力(N, T, kH)を(N, T, H)に変換する線形演算子.
 ※Head数(k)はハイパーパラメータ. 
 """
 
